0a83e7093489d52c9571528c04d49a7dcde2522b
Anna Luiza <luizafistarol@gmail.com>
2023-01-05 14:20:23 -0300

    Adicionando evento de click para exibir o modal em todos os botões

--- js/main.js
+++ js/main.js
@@ -1,22 +1,35 @@
 const semana = document.getElementById("semana");
 let diaSemana = [...document.getElementsByClassName("dia")];
-let btnAdd = [...document.getElementsByClassName("btn-add")];
+let containerDiaSemana = [...document.getElementsByClassName("container-dia-semana")]
 let ulListaItens = document.getElementsByClassName("lista-itens");
 let liItens = document.getElementsByClassName("li-tarefas");
 let btnCheck = document.getElementsByClassName("btn-check");
 const modal = document.getElementById("modal-input");
 const btnAddItem = document.getElementById("btn-add-form");
 let novaTarefa = document.getElementById("input-novo-item");
-let selectDiaSemana = document.querySelector("select")
+let selectDiaSemana = document.querySelector("select");
+let btnsAdd = [...document.getElementsByClassName("btn-add")];
+
+//@ FAZER O MODAL APARECER EM TODOS OS DIAS PARA ADICIONAR ITENS
+btnsAdd.forEach((btn)=>{
+    btn.addEventListener("click", (evento)=>{
+        evento.preventDefault();
+        mostraModal();
+    })
+})
+
+// FUNÇÃO PARA EXIBIR O MODAL
+function mostraModal(){
+    modal.style.display = "block";
+}
 
 
-//CAPTURAR INPUT DO MODAL E ADICIONAR NO DIA DA SEMANA
+//@ CAPTURAR INPUT DO MODAL E ADICIONAR NO DIA DA SEMANA
 btnAddItem.addEventListener("click", (e)=>{
-    e.preventDefault()
-    let nomeNovaTarefa = novaTarefa.value
-    let diaNovaTarefa = selectDiaSemana.value
-
-    criarTarefa(nomeNovaTarefa, diaNovaTarefa)
+    e.preventDefault();
+    let nomeNovaTarefa = novaTarefa.value;
+    let diaNovaTarefa = selectDiaSemana.value;
+    criarTarefa(nomeNovaTarefa, diaNovaTarefa);
 
     //removendo o modal =
     modal.style.display = "none";    
@@ -25,48 +38,43 @@ btnAddItem.addEventListener("click", (e)=>{
     novaTarefa.value = "";
 })
 
+// FUNÇÃO PARA CRIAR TAREFA =
 function criarTarefa(nomeNovaTarefa, diaNovaTarefa){
-    let diaSemanaAdd = document.getElementById(`${diaNovaTarefa}`)
+    let diaSemanaAdd = document.getElementById(`${diaNovaTarefa}`);
 
     //criando a li =
-    const li = document.createElement("li")
-    li.className = "li-tarefas"
+    const li = document.createElement("li");
+    li.className = "li-tarefas";
 
     //criando o botão de check =
-    const checkButton = document.createElement("button")
-    checkButton.className = "btn-check"
-    checkButton.innerHTML = `<i class="fas fa-check"></i>`
+    const checkButton = document.createElement("button");
+    checkButton.className = "btn-check";
+    checkButton.innerHTML = `<i class="fas fa-check"></i>`;
         //<i class="fas fa-check ${obj.completed ? "" : "displayNone"}" data-action="checkButton"></i>`
-    li.appendChild(checkButton)
+    li.appendChild(checkButton);
 
     //criando o texto da li =
-    const p = document.createElement("p")
-    p.className = "nome-tarefa"
-    p.textContent = nomeNovaTarefa
-    li.appendChild(p)
+    const p = document.createElement("p");
+    p.className = "nome-tarefa";
+    p.textContent = nomeNovaTarefa;
+    li.appendChild(p);
 
     //criando o botão de editar =
-    const editButton = document.createElement("i")
-    editButton.className = "fas fa-edit"
-    li.appendChild(editButton)
+    const editButton = document.createElement("i");
+    editButton.className = "fas fa-edit";
+    li.appendChild(editButton);
 
     //criando o botão de excluir =
-    const deleteButton = document.createElement("i")
-    deleteButton.className = "fas fa-trash-alt"
-    li.appendChild(deleteButton)
+    const deleteButton = document.createElement("i");
+    deleteButton.className = "fas fa-trash-alt";
+    li.appendChild(deleteButton);
 
     //adicionando a li na ul correspondente =
-    let adicionar = diaSemanaAdd.parentElement.nextElementSibling
-    adicionar.appendChild(li)
+    let adicionar = diaSemanaAdd.parentElement.nextElementSibling;
+    adicionar.appendChild(li);
 }
 
 
-
-//@ FAZER O MODAL APARECER EM TODOS OS DIAS PARA ADICIONAR ITENS
-    //
-    //btnAdd[0].addEventListener("click", ()=>{
-    //    modal.style.display = "block";
-    //})
 
 //@ FAZER O BOTÃO DE CLOSE DO MODAL FUNCIONAR
 
